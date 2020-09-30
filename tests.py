4fcf89d391f937d2bf27c34dc1414439df96610a
dezalt <dezmont66@mail.ru>
2020-09-30 17:27:02 +0400

    fix3 main.yaml

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import hello as tested_app
 
+
 class FlaskAppTests(unittest.TestCase):
     def setUp(self):
         tested_app.app.config['TESTING'] = True
@@ -9,9 +10,10 @@ class FlaskAppTests(unittest.TestCase):
     def test_get(self):
         r = self.app.get('/')
         self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!')
+
     def test_post(self):
         r = self.app.post('/')
         self.assertEqual(r.status_code, 405)
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
